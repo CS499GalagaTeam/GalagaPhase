45517e83a41a3cd481f902aae3f22b8a4359bf5d
Joshua Matthews <joshuamatthews@jmatth11.local>
2016-10-23 15:47:27 -0500

    Added galaga sprite and displaying it

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
-
+	game.load.image('galaga', './assets/galaga.png');
 }
 
 function create() {
+	game.add.sprite(0,0,'galaga');
 	
 }
 

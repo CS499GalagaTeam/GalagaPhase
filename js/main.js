6d11d4bba2ef47be499cc9aad87e8731192a026c
thestuckster <sstucky3@gmail.com>
2016-10-24 09:59:17 -0500

    more preloads

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 
 function preload() {
 	game.load.image('galaga', './assets/galaga.png');
+	game.load.image('bullet', './assets/images/bullet.png');
+	game.load.image('enemy1', './assets/images/enemy1.png');
+	game.load.image('enemy2', './assets/images/enemy2.png');
+	game.load.image('explosion', './assets/images/explosion.png');
 }
 
 function create() {
 	game.add.sprite(0,0,'galaga');
-	
+
 }
 
 function update() {

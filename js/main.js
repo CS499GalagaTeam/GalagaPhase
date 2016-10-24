e41687597d86f9ee13591ff813e64410156b97d1
thestuckster <sstucky3@gmail.com>
2016-10-24 10:09:44 -0500

    playing around with phaser...

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
+	game.load.image('background', './assets/background.jpg');
 	game.load.image('galaga', './assets/galaga.png');
 	game.load.image('bullet', './assets/images/bullet.png');
 	game.load.image('enemy1', './assets/images/enemy1.png');
@@ -9,8 +10,12 @@ function preload() {
 }
 
 function create() {
-	game.add.sprite(0,0,'galaga');
 
+	game.add.sprite(0,0, 'background');
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	var player =game.add.sprite(0,0,'galaga');
+	game.physics.arcade.enable(player);
 }
 
 function update() {

4a6f31cb82e81ab4530f3c352c9b5050687c0da1
igknighton <trent.knighton@gmail.com>
2016-10-28 11:30:30 -0500

    added ship movement and firing abilityy

--- js/main.js
+++ js/main.js
@@ -9,15 +9,60 @@ function preload() {
 	game.load.image('explosion', './assets/images/explosion.png');
 }
 
+var player;
+var keys;
+var shootButton;
+var bullet;
+var bulletDelay;
+
 function create() {
 
-	game.add.sprite(0,0, 'background');
+	//game.add.sprite(0,0, 'background');
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	var player = game.add.sprite(0.45*600,600-50,'galaga');
+	//creates player
+	player = game.add.sprite(0.45*600,600-50,'galaga');
 	game.physics.arcade.enable(player);
+
+	// allows player to fire 2 bullets 
+	bullet = game.add.weapon(2,'bullet');
+
+	// when bullet leaves the screen, it will be destroyed
+	bullet.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+
+	//offset rotation:
+    bullet.bulletAngleOffset = 90;
+
+    //  The speed at which the bullet is fired
+    bullet.bulletSpeed = 400;
+
+    //  Tell the bullet to track the 'player' Sprite, offset by 16px horizontally, 0 vertically
+    bullet.trackSprite(player, 16, 0);
+
+	//enabling keyboard use
+	keys = game.input.keyboard.createCursorKeys();
+	shootButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update() {
 
+	// Reset the players velocity (movement)
+	player.body.velocity.x = 0;
+
+	if (keys.left.isDown) {
+		// Move to the left
+		player.body.velocity.x = -150;
+	}
+	else if (keys.right.isDown) {
+		// Move to the right
+		player.body.velocity.x = 150;
+	}
+	else  {
+		//ship is idle
+	}
+	//shooting the bullet
+	if (shootButton.isDown) {
+		bullet.fire();
+	}
 }
+

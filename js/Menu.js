34f735adde8ec29c486baa960e4502cd84dffbee
Joshua Matthews <joshuamatthews@jmatth11.local>
2016-11-05 21:48:57 -0500

    Fixed up menu to have moving cursor and fixed up highscore view

--- js/Menu.js
+++ js/Menu.js
@@ -1,4 +1,5 @@
 Menu = function() {
+	option_number = 0;
 }
 
 Menu.prototype = {
@@ -8,49 +9,114 @@ Menu.prototype = {
 },
 
  create: function() {
-
+	// define fonts and positions
+	var mid_x = this.game.width/2;
+	var text_style = {font:"20px Arial", fill:"#ff0000", align:"center"};
+	var option_style = {font:"20px Arial", fill:"#71FFB8", align:"center"};
+	var score_style = {font:"20px Arial", fill:"#ffffff", align:"center"};
+	var cursor_style = {font:"16px Arial", fill:"#ffffff", align:"center"};
+ 	
  	// creates title
- 	var title = this.game.add.sprite(this.game.width/2,200,'title');
+	var title = this.game.add.sprite(mid_x,200,'title');
  	title.anchor.setTo(0.5,0.5);
 	title.scale.setTo(0.6,0.6)
 
- 	// play button
-	var playBtn = this.game.add.button(this.game.width/2,300,'play',this.actionOnClick,this);
-	playBtn.anchor.setTo(0.5,0.5);
-	playBtn.scale.setTo(0.7,0.7);
-
-	var text_style = {font:"20px Arial", fill:"#ff0000", align:"center"};
-	var highScore_text = this.game.add.text(this.game.width/2, 50, "HI-SCORE", text_style);
+	// highscore label text
+	var highScore_text = this.game.add.text(mid_x, 50, "HI-SCORE", text_style);
 	highScore_text.anchor.set(0.5,0.5);
 
-	var score_style = {font:"20px Arial", fill:"#ffffff", align:"center"};
+	// highscore value text
 	var highScore_value = document.getElementById("highscore1").value
-	var highScoreValue_text = this.game.add.text(this.game.width/2, 80, highScore_value, score_style)
+	var highScoreValue_text = this.game.add.text(mid_x, 80, highScore_value, score_style)
 	highScoreValue_text.anchor.set(0.5,0.5)
 
-	var score_button = this.game.add.text(this.game.width/2, 350, "HighScores", text_style)
+ 	// play button
+	playBtn = this.game.add.text(mid_x,300,'Play', option_style);
+	playBtn.anchor.setTo(0.5,0.5);
+	
+	// highscore label button
+	score_button = this.game.add.text(mid_x, playBtn.y + 30, "HighScores", option_style)
 	score_button.anchor.set(0.5,0.5)
-	score_button.inputEnabled = true;
-	score_button.events.onInputDown.add(this.initScoreView, this);
 
+	// credit label button
+	credits_button = this.game.add.text(mid_x, score_button.y + 30, "Credits", option_style);
+	credits_button.anchor.set(0.5,0.5);
 
-	var our_name = this.game.add.text(this.game.width/2, playBtn.y + 90, "The Planeteers", text_style);
+	// current choice triangle label text
+	current_choice = this.game.add.text(playBtn.x - 35, playBtn.y, "\u25B6", cursor_style);
+	current_choice.anchor.set(0.5,0.5);
+
+	// Our name label text
+	var our_name = this.game.add.text(mid_x, score_button.y + 90, "The Planeteers", text_style);
 	our_name.anchor.set(0.5,0.5)
-	
-	
+
+	cursor_down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+	cursor_down.onDown.add(this.selectNextDown);
+	cursor_up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+	cursor_up.onDown.add(this.selectNextUp);
+	choose_option = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	choose_option.onDown.add(this.chooseOption);
 },
 
 update: function() {
 
 },
 
-actionOnClick: function() {
-	this.game.state.start('game',Game);
-},
+	selectNextDown: function() {
+		option_number++;
+		switch(this.option_number) {
+			case 1:
+				this.current_choice.x = score_button.x - 70;
+				this.current_choice.y = score_button.y;
+				break;
+			case 2:
+				this.current_choice.x = credits_button.x - 50;
+				this.current_choice.y = credits_button.y;
+				break;
+			default:
+				this.current_choice.x = playBtn.x - 35;
+				this.current_choice.y = playBtn.y;
+				this.option_number = 0;
+				break;
 
-initScoreView: function() {
-	console.log("starting HighScore!")
-	this.game.state.start('highscore',HighScore);
-}
+		}
+	},
+	
+	selectNextUp: function() {
+		option_number--;
+		switch(this.option_number) {
+			case 0:
+				this.current_choice.x = playBtn.x - 35;
+				this.current_choice.y = playBtn.y;
+				break;
+			case 1:
+				this.current_choice.x = score_button.x - 70;
+				this.current_choice.y = score_button.y;
+				break;
+			default:
+				this.current_choice.x = credits_button.x - 50;
+				this.current_choice.y = credits_button.y;
+				this.option_number = 2;
+				break;
+
+		}
+	},
+
+	chooseOption: function() {
+		switch(option_number) {
+			case 0:
+				this.game.state.start('game', Game);
+				break;
+			case 1:
+				this.game.state.start('highscore', HighScore);
+				break;
+			case 2:
+				this.game.state.start('credit', Credit);
+				break;
+			default:
+				console.log("not an option");
+				break;
+		}
+	},
 
 };
--- js/highscore.js
+++ js/highscore.js
@@ -9,17 +9,24 @@ HighScore.prototype = {
 
 	create:function() {
 		var labels = ["1ST", "2ND", "3RD", "4TH", "5TH"];
-		var topPlayer_style = {font:"20px Arial", fill:"#00ffff", align:"center"}
-		var player_style = {font:"20px Arial", fill:"#0033ff", align:"center"}
+		var topPlayer_style = {font:"20px Arial", fill:"#ffff00", align:"center"}
+		var player_style = {font:"20px Arial", fill:"#71ffB8", align:"center"}
+		var goBack_style = {font:"16px Arial", fill:"#ff0000", align:"center"}
+		var highscore_style = {font:"20px Arial", fill:"#ff0000", align:"center"}
+		var tmp = null;
 		for (var i = 1; i <= 5; ++i) {
 			var tmpScore = document.getElementById("highscore"+i).value
 			var tmpName = document.getElementById("highscore" + i + "name").value
-			var tmp = null;
-			var y_val = (i/6)*this.game.height;
+			tmp = null;
+			var y_val = ((i/10)*this.game.height) + 0.3*this.game.height;
 			if (i == 1) {
 				tmp = this.game.add.text(this.game.width*0.25,y_val, labels[i - 1], topPlayer_style)
 				tmp.anchor.set(0.5,0.5)
+				tmp = this.game.add.text(this.game.width*0.5, y_val - 50, "SCORE", player_style)
+				tmp.anchor.set(0.5,0.5)
 				tmp = this.game.add.text(this.game.width*0.5, y_val, tmpScore, topPlayer_style)
+				tmp.anchor.set(0.5,0.5)
+				tmp = this.game.add.text(this.game.width*0.75, y_val - 50, "NAME", player_style)
 				tmp.anchor.set(0.5,0.5)
 				tmp = this.game.add.text(this.game.width*0.75, y_val, tmpName, topPlayer_style)
 				tmp.anchor.set(0.5,0.5)
@@ -33,15 +40,19 @@ HighScore.prototype = {
 			}
 		}
 
+		var highscore_text = this.game.add.text(this.game.width/2, 0.15*this.game.height, "TOP 5 PLAYERS SCORES", highscore_style);
+		highscore_text.anchor.set(0.5,0.5);
+		var goBack_text = this.game.add.text(this.game.width/2, this.game.height - 20, "press spacebar to go back to main menu", goBack_style);
+		goBack_text.anchor.set(0.5,0.5)
+
+		go_back = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		go_back.onDown.add(function(){game.state.start("menu",Menu);});
+
 	},
 
 
 	update:function() {
 	
 	},
-
-	actionOnClick:function() {
-	
-	}
 
 };
